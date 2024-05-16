3b450ca89eb25e421cb4977578d7136fe08f3bdc
michellelim04 <18221052@std.stei.itb.ac.id>
2024-05-16 20:39:51 +0700

    feat: start login page but styling wont work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ const poppins = Poppins({
 });
 
 export default function Home() {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -25,8 +24,49 @@ export default function Home() {
   };
 
   return (
-    <div>
-      
-    </div>
+    <section
+      className={
+        "min-h-screen flex flex-col justify-center items-center text-center bg-[#2E4F4F] p-16 " +
+        poppins.className
+      }
+    >
+      <Image
+        className="mx-auto"
+        src={"/logo.png"}
+        alt="Logo"
+        width={400}
+        height={400}
+      />
+      <form
+        className="bg-white px-7 py-12 rounded-3xl md:w-[500px]"
+        onSubmit={handleLogin}
+      >
+        <h1 className="mb-5">Login to your account</h1>
+        <input
+          className="bg-[#CBE4DE] px-6 py-3 rounded-full drop-shadow-lg text-sm mb-5 w-full"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          className="bg-[#CBE4DE] px-6 py-3 rounded-full drop-shadow-lg text-sm mb-5 w-full"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button
+          className="bg-[#2E4F4F] text-white px-6 py-3 rounded-full text-sm w-full"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </section>
   );
 }
